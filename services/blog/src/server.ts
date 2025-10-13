3ad76ea761ff141791b0adb66273da1700fda87b
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-10-13 12:54:15 +0545

    feat(blog): initialize blog service with Redis integration

---
+++ services/blog/src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import blogRoutes from "./routes/blog.js";
+import cors from "cors";
+import Redis from "ioredis"
+
+dotenv.config();
+
+export const redisClient = new Redis(process.env.REDIS_URL)
+
+const app = express();
+
+const PORT = process.env.PORT || 5002;
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/v1", blogRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Blog service is running on port ${PORT}`);
+});
