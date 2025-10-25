d7dcf019393e53f7a3854eff5387c3cc6df3b05c
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-10-25 21:15:07 +0545

    feat(home): integrate loading state in Home component for better user experience

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import Loading from "@/components/loading";
 import { Button } from "@/components/ui/button";
+import { useAppData } from "@/context/AppContext";
 
 const Home = () => {
+  const {loading} = useAppData()
   return <div>
-    <Button >Click Me</Button>
-  </div>;
+    {loading ? <Loading/> : <div className="flex items-center justify-center h-screen"><Button>Get Started</Button></div>}
+    </div>;
 };
 
 export default Home;
