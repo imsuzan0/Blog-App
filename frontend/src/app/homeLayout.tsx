43d89d781d666413e0bc7fb3f3602e6f39de9c1c
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-10-29 11:58:12 +0545

    feat(layout): create HomeLayout component with sidebar integration

---
+++ frontend/src/app/homeLayout.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { SidebarProvider } from "../components/ui/sidebar";
+import SideBar from "../components/sidebar";
+
+interface BlogsProps {
+  children: React.ReactNode;
+}
+
+const HomeLayout: React.FC<BlogsProps> = ({ children }) => {
+  return (
+    <div>
+      <SidebarProvider>
+        <SideBar />
+        <main className="w-full">
+          <div className="w-full min-h-[calc(100vh-45)] px-4">{children}</div>
+        </main>
+      </SidebarProvider>
+    </div>
+  );
+};
+
+export default HomeLayout;
