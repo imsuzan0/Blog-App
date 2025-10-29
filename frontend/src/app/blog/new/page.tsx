f6b7bb7f27e93e8234da6b188604969ee66c8109
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-10-29 11:59:29 +0545

    feat(blog): implement AddBlog component for creating new blog posts with AI assistance

---
+++ frontend/src/app/blog/new/page.tsx
@@ -0,0 +1,295 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import dynamic from "next/dynamic";
+import { RefreshCw } from "lucide-react";
+import { useMemo, useRef, useState } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
+import { author_service } from "@/context/AppContext";
+import toast from "react-hot-toast";
+
+const JoditEditor = dynamic(() => import("jodit-react"), { ssr: false });
+
+export const blogCategories = [
+  "Technology",
+  "Health",
+  "Finance",
+  "Politics",
+  "Travel",
+  "Education",
+  "Entertainment",
+  "Sports",
+  "Science",
+  "Business",
+  "Study",
+  "Other",
+];
+
+const AddBlog = () => {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+
+  const config = useMemo(
+    () => ({
+      readonly: false, // all options from https://xdsoft.net/jodit/docs/,
+      placeholder: "Start typings...",
+    }),
+    []
+  );
+
+  const [aiTitle, setAiTitle] = useState(false);
+  const [aiDescription, setAiDescription] = useState(false);
+  const [aiBlogLoading, setAiBlogLoading] = useState(false);
+
+  const [loading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    category: "",
+    image: null,
+    blogcontent: "",
+  });
+
+  const handleInputChange = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFileChange = (e: any) => {
+    const file = e.target.files[0];
+    setFormData({ ...formData, image: file });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const formDataToSend = new FormData();
+
+    formDataToSend.append("title", formData.title);
+    formDataToSend.append("description", formData.description);
+    formDataToSend.append("category", formData.category);
+    formDataToSend.append("blogcontent", formData.blogcontent);
+
+    if (formData.image) {
+      formDataToSend.append("file", formData.image);
+    }
+
+    try {
+      const token = Cookies.get("token");
+      const { data } = await axios.post(
+        `${author_service}/api/v1/blog/new`,
+        formDataToSend,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast.success(data.message);
+      setFormData({
+        title: "",
+        description: "",
+        category: "",
+        image: null,
+        blogcontent: "",
+      });
+      setContent("");
+    } catch (error) {
+      toast.error("Problem while adding blog");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const aiTitleResponse = async () => {
+    try {
+      setAiTitle(true);
+      const { data } = await axios.post(`${author_service}/api/v1/ai/title`, {
+        text: formData.title,
+      });
+      setFormData({ ...formData, title: data.result });
+    } catch (error) {
+      toast.error("Problem while generating title");
+      console.log(error);
+    } finally {
+      setAiTitle(false);
+    }
+  };
+
+  const aiDescriptionResponse = async () => {
+    try {
+      setAiDescription(true);
+      const { data } = await axios.post(
+        `${author_service}/api/v1/ai/description`,
+        {
+          title: formData.title,
+          description: formData.description,
+        }
+      );
+      setFormData({ ...formData, description: data.result });
+    } catch (error) {
+      toast.error("Problem while generating description");
+      console.log(error);
+    } finally {
+      setAiDescription(false);
+    }
+  };
+
+  const aiBlogResponse = async () => {
+    try {
+      setAiBlogLoading(true);
+      const { data } = await axios.post(`${author_service}/api/v1/ai/blog`, {
+        blog: formData.blogcontent,
+      });
+      setContent(data.html);
+      setFormData({ ...formData, blogcontent: data.html });
+    } catch (error: any) {
+      toast.error("Problem while fetching from ai");
+      console.log(error);
+    } finally {
+      setAiBlogLoading(false);
+    }
+  };
+
+  return (
+    <div className="max-w-4xl mx-auto p-6">
+      <Card>
+        <CardHeader>
+          <h2 className="text-2xl font-bold">Add New Blog</h2>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <Label className="font-bold">Title</Label>
+            <div className="flex justify-center items-center gap-2">
+              <Input
+                type="text"
+                value={formData.title}
+                onChange={handleInputChange}
+                name="title"
+                required
+                className={
+                  aiTitle ? "animate-pulse placeholder:opacity-60" : ""
+                }
+                placeholder="Enter blog title"
+              />
+              {formData.title === "" ? (
+                ""
+              ) : (
+                <Button
+                  type="button"
+                  onClick={aiTitleResponse}
+                  disabled={aiTitle}
+                >
+                  <RefreshCw className={aiTitle ? "animate-spin" : ""} />
+                </Button>
+              )}
+            </div>
+
+            <Label>Description</Label>
+            <div className="flex justify-center items-center gap-2">
+              <Input
+                name="description"
+                value={formData.description}
+                onChange={handleInputChange}
+                placeholder="Enter Blog descripiton"
+                className={
+                  aiDescription ? "animate-pulse placeholder:opacity-60" : ""
+                }
+                required
+              />
+              {formData.title === "" ? (
+                ""
+              ) : (
+                <Button
+                  onClick={aiDescriptionResponse}
+                  type="button"
+                  disabled={aiDescription}
+                >
+                  <RefreshCw className={aiDescription ? "animate-spin" : ""} />
+                </Button>
+              )}
+            </div>
+
+            <Label className="font-bold">Category</Label>
+            <Select
+              onValueChange={(value: any) =>
+                setFormData({ ...formData, category: value })
+              }
+            >
+              <SelectTrigger>
+                <SelectValue
+                  placeholder={formData.category || "Select a category"}
+                />
+              </SelectTrigger>
+              <SelectContent>
+                {blogCategories.map((category, index) => (
+                  <SelectItem key={index} value={category}>
+                    {category}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+
+            <div>
+              <Label className="font-bold">Image Upload</Label>
+              <Input type="file" accept="image/*" onChange={handleFileChange} />
+              <p className="text-sm text-muted-foreground">
+                Upload an image for your blog.
+              </p>
+            </div>
+
+            <div>
+              <Label className="font-bold">Blog Content</Label>
+              <div className="flex justify-between items-center mb-2">
+                <p className="text-sm text-muted-foreground">
+                  Paste you blog or type here. You can use rich text formatting.
+                  Please add image after improving your grammer.
+                </p>
+                <Button
+                  onClick={aiBlogResponse}
+                  disabled={aiBlogLoading}
+                  type="button"
+                  size={"sm"}
+                >
+                  <RefreshCw
+                    size={16}
+                    className={aiBlogLoading ? "animate-spin" : ""}
+                  />
+                  <span className="ml-2">Fix Grammer</span>
+                </Button>
+              </div>
+
+              <JoditEditor
+                ref={editor}
+                value={content}
+                config={config}
+                tabIndex={1}
+                onBlur={(newContent) => {
+                  setContent(newContent);
+                  setFormData({ ...formData, blogcontent: newContent });
+                }}
+              />
+            </div>
+            <Button type="submit" className="w-full" disabled={loading}>
+              {loading ? "Submitting..." : "Submit"}
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default AddBlog;
