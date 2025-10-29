f2c4453cefab1d184c70bd16e2546e5ff496b54d
Sujan Timalsina <sujantimalsina333@gmail.com>
2025-10-29 11:57:48 +0545

    feat(context): add blog fetching functionality with search and category filters

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -45,6 +45,12 @@ interface AppContextType {
   setIsAuth: React.Dispatch<React.SetStateAction<boolean>>;
   setUser: React.Dispatch<React.SetStateAction<User | null>>;
   logOutUser: () => void;
+  blogs: Blog[] | null;
+  blogLoading: boolean;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
+  searchQuery: string;
+  category: string;
+  setCategory: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
@@ -63,7 +69,7 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
         return;
       }
 
-      const { data } = await axios.get(`${user_service}/api/v1/me`, {
+      const { data }: any = await axios.get(`${user_service}/api/v1/me`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
@@ -79,6 +85,37 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
     }
   }
 
+  const [blogLoading, setBlogLoading] = useState(true);
+  const [blogs, setBlogs] = useState<Blog[] | null>(null);
+
+  const [category, setCategory] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  async function fetchBlogs() {
+    setBlogLoading(true);
+    try {
+      const { data }: any = await axios.get(
+        `${blog_service}/api/v1/blog/all?searchQuery=${searchQuery}&category=${category}`
+      );
+      if (Array.isArray(data)) {
+        setBlogs(data);
+        console.log("Fetched blogs:", data);
+      } else if (data.blogs && Array.isArray(data.blogs)) {
+        setBlogs(data.blogs);
+        console.log("Fetched blogs:", data.blogs);
+      } else {
+        setBlogs([]);
+        console.log("No blogs found");
+      }
+      console.log("All Blog", data.blogs);
+      console.log("Fetched blogs:", data.blogs);
+    } catch (error) {
+      console.log("Error fetching blog:", error);
+    } finally {
+      setBlogLoading(false);
+    }
+  }
+
   async function logOutUser() {
     Cookies.remove("token");
     setIsAuth(false);
@@ -90,6 +127,10 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
     fetchUser();
   }, []);
 
+  useEffect(() => {
+    fetchBlogs();
+  }, [category, searchQuery]);
+
   return (
     <AppContext.Provider
       value={{
@@ -100,6 +141,12 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
         setIsAuth,
         setUser,
         logOutUser,
+        blogs,
+        blogLoading,
+        setSearchQuery,
+        searchQuery,
+        category,
+        setCategory,
       }}
     >
       <GoogleOAuthProvider clientId="683718027551-q6q8klh8mvr34p90hr0u11lt14cv01jg.apps.googleusercontent.com">
